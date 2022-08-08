cebeb8fbd40a53757ff036d560477b4cb8368e50
Kateryna Borysenko <81910238+Kateryna-Borysenko@users.noreply.github.com>
2022-08-08 14:31:18 +0300

    Update 02-arrays.ts

--- src/02-arrays.ts
+++ src/02-arrays.ts
@@ -1,21 +1,28 @@
 /*
  * Массивы
+ 
  *  - Нотации `тип[]` и `Array<тип>`
+ const temps:number[] = [30, 31, 27, 28, 32];
+  const temps:Array<number>[] = [30, 31, 27, 28, 32]; - джинерики
+  
  *  - `(тип | тип)[]`
+ можно записать если несколько типов, но обычно так не делают тк массив это коллекция однотипных данных
+ const temps1: (number | string )[] = [30, 31, 27, 28, 32, 'test1'];
+ 
  *  - `readonly` и `ReadonlyArray<тип>`
+ const temps:number[] = [30, 31, 27, 28, 32];
+ 
  *  - Tuple `[тип, тип]`, например массив координат
  */
 
-const temps: readonly number[] = [30, 31, 27, 28, 32];
+
+
+const temps: readonly number[] = [30, 31, 27, 28, 32]; //readonly - ключевое слово ке делает массив только для чтения
 
 // temps.push(5);
 
-const coords: [number, number] = [50.4501, 30.5234];
+const coords: [number, number] = [50.4501, 30.5234]; //когда массив с переменными в определённой последовательности -> массив из двух переменных каждая из которых явл числом
 const rgb: [number, number, number] = [255, 0, 100];
-
-const arrA = [1, 2, 3, 4, 5];
-
-const arrB = [...arrA];
 
 console.log(temps, coords, rgb);
 
