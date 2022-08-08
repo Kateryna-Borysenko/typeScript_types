fcf8ac33b51d651f0ccee2c52767824cb697c8d4
Kateryna Borysenko <81910238+Kateryna-Borysenko@users.noreply.github.com>
2022-08-08 14:39:47 +0300

    Update 03-types.ts

--- src/03-types.ts
+++ src/03-types.ts
@@ -3,17 +3,17 @@
  * - Идентификатор c type composition
  * - Состояние с union
  */
-
-type ID = number | string;
+//объявляется тип -> говорим что там будет лежать (ID это алиос - псевдоним для такого типа)
+type ID = number | string; //в тайпе id может лежать композиция типов или string или number
 
 const userId: ID = 5555;
 const postId: ID = "f7ln24m91z";
 
-type Coords = [number, number];
+type Coords = [number, number]; //!важно писать тип с большой буквы
 
 const coords: Coords = [50.4501, 30.5234];
 
-type ReqStatus = "request" | "success" | "error";
+type ReqStatus = "request" | "success" | "error"; //это юниан когда у переменной может быть одно из нескольких конкретных значениий
 
 const requestStatus: ReqStatus = "success";
 
