42ed71486856a63967afe5c324e15f810ab85464
Kateryna Borysenko <81910238+Kateryna-Borysenko@users.noreply.github.com>
2022-08-08 15:01:55 +0300

    Update 04-interfaces.ts

--- src/04-interfaces.ts
+++ src/04-interfaces.ts
@@ -1,5 +1,5 @@
 /*
- * Интерфейсы
+ * Интерфейсы - используются для того чтобы описать объект
  *  - Конструкция `interface`
  *  - Необзательные поля с `key?: value`
  *  - readonly поля с `readonly key: value`
@@ -9,24 +9,26 @@
  *  - [interface vs type](https://pawelgrzybek.com/typescript-interface-vs-type/)
  */
 
+
 interface PluginConfig {
-  readonly selector: string;
+  readonly selector: string; //readonly - свойство которое можно только прочитать
   perPage?: number;
-  startIndex?: number;
-  draggable?: boolean;
+  startIndex?: number; //? для того что бы в объекте объявить не обязательное свойство
+  draggable?: boolean;  
 }
 
 const config: PluginConfig = {
   selector: "#plugin-1",
   perPage: 2,
-  // startIndex: 0,
-  // draggable: false,
+  startIndex: 0,
+  draggable: false,
 };
 
 // console.log(config);
 
+//для описания объекта с неизвестным кол-вом свойств, но явн задаными типами
 interface Employees {
-  [key: string]: number;
+  [key: string]: number; //как будто вычисляемыми свойствами ключ это строка значения это числа
 }
 
 const employees: Employees = {
@@ -35,6 +37,10 @@ const employees: Employees = {
   ajax: 15,
   wiki: 20,
 };
+
+//----------------
+//находим самого продуктивного работника 
+//ts заинферил все типы при решении задачи и не понадобилось писать типы
 
 const entries = Object.entries(employees);
 let bestEmployeeName = "";
